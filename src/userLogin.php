a7ba01b23586893d566d2a327574459c217ab8e8
Efe Şaman <efe.saman@ug.bilkent.edu.tr>
2022-04-22 21:32:28 +0300

    Update userLogin.php

--- src/userLogin.php
+++ src/userLogin.php
@@ -10,51 +10,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="CSS/style.css">
 
-  <?php
-  include("config.php");
-  function login($mysqli) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-
-    if ($username == "" && $password == "") {
-      echo '<script> alert("Please enter your username and password") </script>';
-      return;
-    }
-    else if ($username == "") {
-        echo '<script> alert("Please enter your username") </script>';
-        return;
-    }
-    else if ($password == "") {
-      echo '<script> alert("Please enter your password") </script>';
-      return;
-    }
-
-    $rs = $mysqli->query("" .
-                         "SELECT * " .
-                         "FROM User " .
-                         "WHERE username = '$username' AND password = '$password' ");
-
-    // login is successful
-    if ($rs) {
-      if ($rs->num_rows > 0) {
-        $_SESSION['username'] = $username;
-        header("location: userHomePage.php");
-      }
-      else {
-        echo '<script> alert(Wrong input!") </script>';
-        header("location: index.php");
-      }
-    }
-    else {
-      // Error with the query
-      header("location: index.php");
-    }
-  }
-
-  if (isset($_POST['login'])) {
-    login($mysqli);
-  }
-  ?>
 </head>
 
 <body>
@@ -73,7 +28,52 @@
           <h2>Welcome back! If you are a user, login here.</h2>
         </div>
 
-        <form action="login.php" method="POST" id="login">
+        <?php
+        include("config.php");
+        function login($mysqli) {
+          $username = $_POST['username'];
+          $password = $_POST['password'];
+
+          if ($username == "" && $password == "") {
+            echo '<script> alert("Please enter your username and password") </script>';
+            return;
+          }
+          else if ($username == "") {
+              echo '<script> alert("Please enter your username") </script>';
+              return;
+          }
+          else if ($password == "") {
+            echo '<script> alert("Please enter your password") </script>';
+            return;
+          }
+
+          $rs = $mysqli->query("" .
+                               "SELECT * " .
+                               "FROM User " .
+                               "WHERE username = '$username' AND password = '$password' ");
+
+          // login is successful
+          if ($rs) {
+            if ($rs->num_rows > 0) {
+              $_SESSION['username'] = $username;
+              header("location: userHomePage.php");
+            }
+            else {
+              echo '<script> alert("Wrong input!") </script>';
+            }
+          }
+          else {
+            // Error with the query
+            header("location: userLogin.php");
+          }
+        }
+
+        if (isset($_POST['login'])) {
+          login($mysqli);
+        }
+        ?>
+
+        <form action="userLogin.php" method="POST" id="login">
           <div class="forms">
             <input type="text" class="data-box" placeholder="Username" name="username">
             <input type="password" class="data-box" placeholder="Password" name="password">
